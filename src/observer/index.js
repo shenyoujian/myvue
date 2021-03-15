f9d191369c3f224092db687c6e8fbbf77a0de7bf
shenyoujian2 <shenyoujian2@163.com>
2021-03-15 22:36:22 +0800

    vue对象类型的劫持

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,3 +1,48 @@
-export function observe(data) {
-  console.log(data)
+// 
+class Observer {
+  constructor(value) {
+    // 3、进行劫持，使用Object.definedProperty重新定义属性
+    this.walk(value)
+  }
+
+  walk(data) {
+    // 4、循环遍历data，先遍历第一层
+    let keys = Object.keys(data)
+    keys.forEach(key => {
+      let value = data[key]
+      // 5、把劫持下来的数据变成响应式后重新设置到data里
+      defineReactive(data, key, value)    //defineReactive这个方法在vue中可以直接用的，Vue.util.defineReactive,这个方法的作用是把数据变成响应式的数据
+    })
+  }
+
 }
+
+
+function defineReactive(data, key, value) {
+  // 7、递归进行劫持
+  observe(value)
+  //6、Object.definedProperty
+  Object.defineProperty(data,key,{
+    get() {
+      console.log('用户获取值了')
+      return value
+    },
+    set(newvalue) {
+      console.log('用户设置值了')
+      // 如果跟原来的值那就无事发生
+      if(newvalue === value) return 
+      observe(newvalue)   // 如果用户将值改为对象继续监控
+      value = newvalue
+      
+    }
+  })
+}
+
+export function observe(data) {
+  // 1、我们规定data只能传函数或者对象，但是别人可能会乱传，所以先data可能先判断data是否是对象
+  if (typeof data !== 'object' || data === null) return
+
+  // 2、观测对象或者数组等，这些方法耦合在一起，这时候就得用类了
+  return new Observer(data)
+}
+
--- src/state.js
+++ src/state.js
@@ -28,7 +28,8 @@ function initData(vm) {    // 数据的初始化操作
   let data = vm.$options.data
   // data可能是对象也可能是一个函数，当它是函数获取它的返回值，因为我们要对对象进行劫持
   // call vm.data()  
-  data = typeof data == "function" ? data.call(vm) : data
+  // 为了能在index.html里的vue实例可以操作data里的数据，把data的引用赋值给vue实例上
+  vm._data = data = typeof data == "function" ? data.call(vm) : data
 
   // 数据劫持的方案 对象Object.defineProperty
   // 数组单独处理
